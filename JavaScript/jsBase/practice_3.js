8bc59f99a514c6375beed84092efe2c40dcc4555
masya-nya <89185487468m@gmail.com>
2021-02-12 22:53:14 +0300

    12.02.2021

---
+++ JavaScript/jsBase/practice_3.js
@@ -0,0 +1,66 @@
+'use strict'
+/*
+let browser = 'chrome'
+
+if (browser === 'edge') {
+    console.log("You've got the Edge!")
+} else if (browser === 'chrome'
+    || browser === 'firefox'
+    || browser === 'safari'
+    || browser === 'Opera') {
+    console.log('Okay we support these browsers too')
+} else {
+    console.log('We hope that this page looks ok!')
+}
+
+const number = 0
+switch (number) {
+    case 0:
+        console.log('Вы ввели 0')
+        break
+    case 1:
+        console.log('Вы ввели 1')
+        break
+    case 2:
+    case 3:
+        console.log('Вы ввели 2 или 3')
+        break
+    default:
+        console.log('Что вы вообще ввели?')
+}
+*/
+/*
+function sayHi() {
+    console.log('Hi')
+}
+const sayHello = function () {
+    console.log('Hello')
+}
+const welcome = () => {
+    console.log('Welcome')
+}
+let one = sayHi
+let two = sayHello
+let three = welcome
+sayHi()
+sayHello()
+welcome()
+one()
+two()
+three()
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no()
+}
+
+ask('Хотите ли вы продолжить?',
+    () => { alert('Вы согласны!') },
+    () => { alert('Вы не согласны!') })
+*/
+let users = {
+    name: 'Максим',
+    sizes: {
+        height: '187',
+        width: '50'
+    }
+}
---
+++ Practice/Project_3/JS/script.js
@@ -0,0 +1,90 @@
+new Swiper('.testimonials__slider', {
+    //Буллеты
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnIteraction: true,
+    },
+    speed: 800,
+});
+
+const gallery = document.querySelector('.gallery')
+gallery.addEventListener('mouseover', (e) => {
+    let target = e.target
+})
+
+const galleryNav = document.querySelector('.portfolio__nav')
+const galleryLi = document.querySelectorAll('.portfolio__nav > li')
+const galleryItem = document.querySelectorAll('.gallery__item')
+galleryNav.addEventListener('click', (e) => {
+    let target = e.target
+    if (target.tagName === 'LI') {
+        console.log(target)
+        if (!target.classList.contains('active-category')) {
+            for (item of galleryLi) {
+                if (item.classList.contains('active-category')) {
+                    item.classList.remove('active-category')
+                }
+            }
+            target.classList.add('active-category')
+            if (target.innerHTML === 'All') {
+                for (item of galleryItem) {
+                    if (item.classList.contains('gallery__item')) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            }
+            if (target.innerHTML === 'Logo') {
+                for (item of galleryItem) {
+                    if (item.classList.contains('category-logo')) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            }
+            if (target.innerHTML === 'Branding') {
+                for (item of galleryItem) {
+                    if (item.classList.contains('category-branding')) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            }
+            if (target.innerHTML === 'Web') {
+                for (item of galleryItem) {
+                    if (item.classList.contains('category-web')) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            }
+            if (target.innerHTML === 'Photography') {
+                for (item of galleryItem) {
+                    if (item.classList.contains('category-photography')) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            }
+            if (target.innerHTML === 'Poster') {
+                for (item of galleryItem) {
+                    if (item.classList.contains('category-poster')) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            }
+        }
+    }
+})
